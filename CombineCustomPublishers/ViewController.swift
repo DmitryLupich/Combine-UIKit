2606b9c8a393f53235102d1e007573a9aaa81196
Dmytriy <dmytro.lupych@sixt.com>
2020-02-14 10:12:01 +0200

    Initial Commit

---
+++ CombineCustomPublishers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CombineCustomPublishers
+//
+//  Created by Dmitriy Lupych on 14.02.2020.
+//  Copyright © 2020 Dmitry Lupich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
